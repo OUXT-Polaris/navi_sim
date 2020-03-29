cefab23654518fd0242eb6dfda3fcde180683c50
Masaya Kataoka <ms.kataoka@gmail.com>
2020-03-29 19:13:12 +0900

    fix include order

--- navi_sim/src/navi_sim_component.cpp
+++ navi_sim/src/navi_sim_component.cpp
@@ -1,4 +1,21 @@
+// Copyright (c) 2020 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <navi_sim/navi_sim_component.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <set>
+#include <vector>
 
 namespace navi_sim
 {
@@ -195,7 +212,6 @@ void NaviSimComponent::updateScan()
   laser_pub_->publish(scan);
   mtx_.unlock();
 }
-}
+} // namespace navi_sim
 
-#include <rclcpp_components/register_node_macro.hpp>
 RCLCPP_COMPONENTS_REGISTER_NODE(navi_sim::NaviSimComponent)
