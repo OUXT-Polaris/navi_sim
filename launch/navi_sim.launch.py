06c23e3e58f07fd031eaa1273e0279bb3c9f3f54
Masaya Kataoka <ms.kataoka@gmail.com>
2020-03-30 10:06:41 +0900

    enable pass ament_flake8

--- launch/navi_sim.launch.py
+++ launch/navi_sim.launch.py
@@ -17,9 +17,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+
 
 def generate_launch_description():
     rviz_config_dir = os.path.join(
--- launch/with_planner.launch.py
+++ launch/with_planner.launch.py
@@ -16,20 +16,18 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
-from launch.actions import ExecuteProcess,DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
     navi_sim_launch_file_dir = os.path.join(get_package_share_directory('navi_sim'), 'launch')
-    hermite_path_planner_bringup_launch_file_dir = os.path.join(get_package_share_directory('hermite_path_planner_bringup'), 'launch')
+    package_path = get_package_share_directory('hermite_path_planner_bringup')
+    launch_dir = os.path.join(package_path, 'launch')
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([navi_sim_launch_file_dir, '/navi_sim.launch.py']),
         ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([hermite_path_planner_bringup_launch_file_dir, '/bringup.launch.py']),
+            PythonLaunchDescriptionSource([launch_dir, '/bringup.launch.py']),
         )
     ])
