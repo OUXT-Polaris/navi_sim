7cc3fda49ba5fbc40134f6d08ad5f19f2bffdfe0
Masaya Kataoka <ms.kataoka@gmail.com>
2021-09-08 12:36:57 +0900

    comment out deleted field
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- src/camera_sim_component.cpp
+++ src/camera_sim_component.cpp
@@ -96,14 +96,14 @@ void CameraSimComponent::update()
       vision_msgs::msg::Detection2D detection;
       detection.header.frame_id = camera_optical_frame_;
       detection.header.stamp = now;
-      detection.is_tracking = false;
+      // detection.is_tracking = false;
       detection.bbox.center.x = (out.max_corner().x() + out.min_corner().x()) * 0.5;
       detection.bbox.center.y = (out.max_corner().y() + out.min_corner().y()) * 0.5;
       detection.bbox.size_x = out.max_corner().x() - out.min_corner().x();
       detection.bbox.size_y = out.max_corner().y() - out.min_corner().y();
       vision_msgs::msg::ObjectHypothesisWithPose result;
-      result.id = raycaster_ptr_->getObjectType(name);
-      result.score = 1.0;
+      // result.id = raycaster_ptr_->getObjectType(name);
+      // result.score = 1.0;
       detection.results.emplace_back(result);
       detection_array.detections.emplace_back(detection);
     }
