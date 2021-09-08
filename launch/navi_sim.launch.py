2f3bfdb44af95e3a38c774fa22ae05537e49a024
Masaya Kataoka <ms.kataoka@gmail.com>
2021-09-08 09:41:50 +0900

    fix some errors in launch file
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- launch/navi_sim.launch.py
+++ launch/navi_sim.launch.py
@@ -14,6 +14,7 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
 from launch.actions import EmitEvent, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
@@ -22,7 +23,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 from launch.substitutions.launch_configuration import LaunchConfiguration
-from launch.conditions import IfCondition
 
 
 import os
