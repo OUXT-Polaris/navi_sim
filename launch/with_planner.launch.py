f94d6cc709f302da818948b04bd7e483ddbcdc10
MasayaKataoka <ms.kataoka@gmail.com>
2022-08-27 23:13:23 +0900

    add argument
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- launch/with_planner.launch.py
+++ launch/with_planner.launch.py
@@ -24,42 +24,77 @@ import os
 
 
 def generate_launch_description():
-    navi_sim_launch_file_dir = os.path.join(get_package_share_directory('navi_sim'), 'launch')
-    planner_package_path = get_package_share_directory('robotx_planner_bringup')
-    planner_launch_dir = os.path.join(planner_package_path, 'launch')
-    perception_bringup_package_path = get_package_share_directory('perception_bringup')
-    perception_bringup_launch_dir = os.path.join(perception_bringup_package_path, 'launch')
-    scenario_filename = LaunchConfiguration('scenario_filename', default='go_straight.yaml')
-    scenario_mode = LaunchConfiguration('scenario_mode', default=False)
-    record = LaunchConfiguration('record', default=False)
-    rosbag_directory = LaunchConfiguration('rosbag_directory', default='/tmp')
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'scenario_filename',
-            default_value=scenario_filename,
-            description='filename of the scenario yaml file.'),
-        DeclareLaunchArgument(
-            'scenario_mode',
-            default_value=scenario_mode,
-            description='If true, running with scenario_mode'),
-        DeclareLaunchArgument(
-            'record',
-            default_value=record,
-            description='If true, record rosbag data.'),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [navi_sim_launch_file_dir, '/navi_sim.launch.py']),
-            launch_arguments={
-                'scenario_filename': scenario_filename,
-                'record': record,
-                'rosbag_directory': rosbag_directory}.items()
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [planner_launch_dir, '/planner_bringup.launch.py'])
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [perception_bringup_launch_dir, '/perception_bringup.launch.py'])
-        )
-    ])
+    navi_sim_launch_file_dir = os.path.join(
+        get_package_share_directory("navi_sim"), "launch"
+    )
+    planner_package_path = get_package_share_directory("robotx_planner_bringup")
+    planner_launch_dir = os.path.join(planner_package_path, "launch")
+    perception_bringup_package_path = get_package_share_directory("perception_bringup")
+    perception_bringup_launch_dir = os.path.join(
+        perception_bringup_package_path, "launch"
+    )
+    scenario_filename = LaunchConfiguration(
+        "scenario_filename", default="go_straight.yaml"
+    )
+    scenario_mode = LaunchConfiguration("scenario_mode", default=False)
+    record = LaunchConfiguration("record", default=False)
+    rosbag_directory = LaunchConfiguration("rosbag_directory", default="/tmp")
+    behavior_config_package = LaunchConfiguration(
+        "behavior_config_package", default="robotx_bt_planner"
+    )
+    behavior_config_filepath = LaunchConfiguration(
+        "behavior_config_filepath", default="config/go.yaml"
+    )
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "scenario_filename",
+                default_value=scenario_filename,
+                description="filename of the scenario yaml file.",
+            ),
+            DeclareLaunchArgument(
+                "scenario_mode",
+                default_value=scenario_mode,
+                description="If true, running with scenario_mode",
+            ),
+            DeclareLaunchArgument(
+                "record",
+                default_value=record,
+                description="If true, record rosbag data.",
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [navi_sim_launch_file_dir, "/navi_sim.launch.py"]
+                ),
+                launch_arguments={
+                    "scenario_filename": scenario_filename,
+                    "record": record,
+                    "rosbag_directory": rosbag_directory,
+                }.items(),
+            ),
+            DeclareLaunchArgument(
+                "behavior_config_package",
+                default_value=behavior_config_package,
+                description="package of behavior config file",
+            ),
+            DeclareLaunchArgument(
+                "behavior_config_filepath",
+                default_value=behavior_config_filepath,
+                description="filepath of the behavior config",
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [planner_launch_dir, "/planner_bringup.launch.py"]
+                ),
+                launch_arguments={
+                    "behavior_config_package": behavior_config_package,
+                    "behavior_config_filepath": behavior_config_filepath,
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [perception_bringup_launch_dir, "/perception_bringup.launch.py"]
+                )
+            ),
+        ]
+    )
